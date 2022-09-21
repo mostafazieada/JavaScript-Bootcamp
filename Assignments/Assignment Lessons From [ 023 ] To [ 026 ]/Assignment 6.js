e3e17a03ac08a02619907d442fa3083e8299b372
mostafazieada <77005290+mostafazieada@users.noreply.github.com>
2022-09-21 08:55:54 +0200

    error in the first solution
    the result of the first solution will be float (==>10.4)
    and you missed up with the variable name 
    i hope that would helpe you pro
    cause you have helped me alot

--- Assignments/Assignment Lessons From [ 023 ] To [ 026 ]/Assignment 6.js
+++ Assignments/Assignment Lessons From [ 023 ] To [ 026 ]/Assignment 6.js
@@ -1,7 +1,7 @@
 let flt = 10.4;
 
-console.log(Number(num)); // 10
+console.log(Number(flt)); // 10
 console.log(Number.parseInt(flt)); // 10
-console.log(Math.round(num)); // 10
-console.log(Math.floor(num)); // 10
-console.log(Math.trunc(num)); // 10
+console.log(Math.round(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.trunc(flt)); // 10
